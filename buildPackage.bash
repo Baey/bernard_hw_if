463f440f1e65f60876787d866d0d267ef696f10a
Błażej <blazejszargut@gmail.com>
2025-12-01 00:04:43 +0100

    Enhance buildPackage.bash to accept build type
    Added support for specifying build type as an argument.

--- buildPackage.bash
+++ buildPackage.bash
@@ -3,16 +3,18 @@
 set -e
 
 # --- ROS DISTRO ---
-if [ $# -ge 1 ]; then
-    DISTRO="$1"
-elif [ -n "${ROS_DISTRO:-}" ]; then
-    DISTRO="$ROS_DISTRO"
-    echo "Using currently sourced ROS distro: $DISTRO"
+if [ $# -lt 2 ]; then
+    CMAKE_BUILD_TYPE="Debug"
 else
-    echo "Error: No ROS distribution specified and ROS_DISTRO not set."
-    echo "Usage: $0 <ROS_DISTRO> [build_type]"
-    echo "Example: $0 humble release"
-    exit 1
+    arg=$(echo "$2" | tr '[:upper:]' '[:lower:]')
+    if [ "$arg" == "release" ]; then
+        CMAKE_BUILD_TYPE="Release"
+    elif [ "$arg" == "debug" ]; then
+        CMAKE_BUILD_TYPE="Debug"
+    else
+        echo "Unknown build type: $2"
+        exit 1
+    fi
 fi
 
 # Source ROS
